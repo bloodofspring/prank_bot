34e33ecd07d67baf96f4a2130b871ef131ec5f8b
bloodofspring <t.shagin@gmail.com>
2024-11-11 19:31:10 +0300

    feat: Удалены лишние модули

--- client_handlers/__init__.py
+++ client_handlers/__init__.py
@@ -1,7 +1,6 @@
 from client_handlers.buttons_h import HowDItWorks, GetLink
 from client_handlers.dump_cmd import Dump
 from client_handlers.mailing import Mailing
-from client_handlers.settings import OpSettings, ChangeOpConfig, ChannelUsernameDownloader
 from client_handlers.start import StartCmd
 
 active_handlers = [
@@ -9,8 +8,5 @@ active_handlers = [
     Mailing,
     HowDItWorks,
     GetLink,
-    OpSettings,
-    ChangeOpConfig,
-    ChannelUsernameDownloader,
     Dump,
 ]
