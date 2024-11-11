e156345cf8924fe19f62ef2a060aa48a7eea9522
Tim Ross <149096639+bloodofspring@users.noreply.github.com>
2024-11-11 19:36:35 +0300

    Update util.py
    Исправлена ошибка

--- util.py
+++ util.py
@@ -3,23 +3,6 @@ from datetime import datetime
 from colorama import Fore
 from pyrogram.types import InlineKeyboardButton
 
-from database.models import ChannelsToSub
-
-
-def all_op_keyboard(remove: bool):
-    keyboard = []
-
-    for chan in ChannelsToSub.select():
-        if remove:
-            keyboard.append([InlineKeyboardButton(text=f"Канал {chan.ID}", callback_data=f"rem_op {chan.tg_id}")])
-            continue
-
-        keyboard.append([
-            InlineKeyboardButton(text=f"Канал {chan.ID} ({chan.tg_id})", url=f"https://t.me/{chan.tg_id.strip('@')}")
-        ])
-
-    return keyboard
-
 
 def color_log(text: str, colors: str | list[str], head_c: str = Fore.LIGHTWHITE_EX, separator: str = " ") -> str:
     now = datetime.now()
