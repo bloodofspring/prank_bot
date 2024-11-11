1a12cc759b30862b37716c03a9f9bde011d138c4
bloodofspring <t.shagin@gmail.com>
2024-11-11 18:28:15 +0300

    feat: Подключен обработчик DumpCmd

--- client_handlers/__init__.py
+++ client_handlers/__init__.py
@@ -1,4 +1,5 @@
 from client_handlers.buttons_h import HowDItWorks, GetLink
+from client_handlers.dump_cmd import Dump
 from client_handlers.mailing import Mailing
 from client_handlers.settings import OpSettings, ChangeOpConfig, ChannelUsernameDownloader
 from client_handlers.start import StartCmd
@@ -11,4 +12,5 @@ active_handlers = [
     OpSettings,
     ChangeOpConfig,
     ChannelUsernameDownloader,
+    Dump,
 ]
