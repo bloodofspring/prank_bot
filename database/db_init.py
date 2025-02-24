a901caf2e974ce51d048b4075e91bd6532b3d6bc
bloodofspring <t.shagin@gmail.com>
2025-02-24 08:04:16 +0300

    fix: Добавлено автоматическое переподключение к базе данных

--- database/db_init.py
+++ database/db_init.py
@@ -3,9 +3,13 @@ from typing import Final
 from peewee import PostgresqlDatabase
 
 from config import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+from playhouse.shortcuts import ReconnectMixin
 
-psql_db: Final[PostgresqlDatabase] = PostgresqlDatabase(
-    "prankBotDb",
+class ReconnectPSQLDatabase(ReconnectMixin, PostgresqlDatabase):
+    pass
+
+psql_db: Final[ReconnectPSQLDatabase] = ReconnectPSQLDatabase(
+    "testDb",  # prankBotDb
     user=DB_USER,
     password=DB_PASSWORD,
     host=DB_HOST,
